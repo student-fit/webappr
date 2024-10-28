f3c8609f24466117476151ab92732391f344ec00
Husein Marić <79978304+marichusein@users.noreply.github.com>
2024-10-28 18:55:37 +0100

    fixed

--- WebStranica/webpage/src/components/Navigation.js
+++ WebStranica/webpage/src/components/Navigation.js
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import './Navigation.css'; // Učitajte CSS fajl za stilizaciju navigacije
-import Logoo from '../logo.png'; // Import your logo component or image
-
+import './Navigation.css';
+import Logoo from '../logo.png';
 
 const Navigation = ({ i18n }) => {
   const { t } = useTranslation();
   const [languageMenuOpen, setLanguageMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); // State za mobilni meni
 
   const toggleLanguageMenu = () => {
     setLanguageMenuOpen(!languageMenuOpen);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Funkcija za otvaranje/zatvaranje mobilnog menija
+  };
+
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
-    setLanguageMenuOpen(false); // Zatvara padajući meni nakon promene jezika
+    setLanguageMenuOpen(false);
   };
 
   return (
     <nav>
-      {/* Desktop navigacija */}
       <div className="logo">
-      <a href="/">
-        <img className="logo" src={Logoo} alt="Logo" />
+        <a href="/">
+          <img className="logo" src={Logoo} alt="Logo" />
         </a>
       </div>
 
+      {/* Desktop navigacija */}
       <ul className="desktop-nav">
         <li><a className="linkic" href="/">{t('home')}</a></li>
         <li><a className="linkic" href="/taxi">{t('taxi')}</a></li>
         <li><a className="linkic" href="/transfers">{t('transfers')}</a></li>
         <li><a className="linkic" href="/tours">{t('tours')}</a></li>
         <li><a className="linkic" href="/contact-us">{t('contact_us')}</a></li>
-
-        {/* <li><a href="/accommodation">{t('accommodation')}</a></li> */}
         <li className="language-menu">
           <button onClick={toggleLanguageMenu}>{t('language')}</button>
           {languageMenuOpen && (
@@ -43,10 +45,6 @@ const Navigation = ({ i18n }) => {
               <li><button onClick={() => changeLanguage('es')}>ES</button></li>
               <li><button onClick={() => changeLanguage('fr')}>FR</button></li>
               <li><button onClick={() => changeLanguage('it')}>IT</button></li>
-
-
-
-              {/* Dodajte ostale opcije za promenu jezika */}
             </ul>
           )}
         </li>
@@ -54,7 +52,33 @@ const Navigation = ({ i18n }) => {
 
       {/* Mobilna navigacija (hamburger meni) */}
       <div className="mobile-nav">
-        {/* Implementacija hamburger menija */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {menuOpen && (
+          <ul className="menu">
+            <li><a className="linkic" href="/">{t('home')}</a></li>
+            <li><a className="linkic" href="/taxi">{t('taxi')}</a></li>
+            <li><a className="linkic" href="/transfers">{t('transfers')}</a></li>
+            <li><a className="linkic" href="/tours">{t('tours')}</a></li>
+            <li><a className="linkic" href="/contact-us">{t('contact_us')}</a></li>
+            <li className="language-menu">
+              <button onClick={toggleLanguageMenu}>{t('language')}</button>
+              {languageMenuOpen && (
+                <ul className="language-dropdown">
+                  <li><button onClick={() => changeLanguage('en')}>EN</button></li>
+                  <li><button onClick={() => changeLanguage('hr')}>HR</button></li>
+                  <li><button onClick={() => changeLanguage('es')}>ES</button></li>
+                  <li><button onClick={() => changeLanguage('fr')}>FR</button></li>
+                  <li><button onClick={() => changeLanguage('it')}>IT</button></li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
